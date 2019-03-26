7f519f5a26f3fbd2e0b345d0e84ff449b13f3823
Fatlonder <fatlondercakolli@gmail.com>
2019-03-26 12:22:18 +0100

    initial commit: .gitignore

--- src/main/java/com/springframework/dao/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springframework.dao;
-
-import com.springframework.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
